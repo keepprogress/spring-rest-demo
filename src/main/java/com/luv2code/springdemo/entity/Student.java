fabdcc3507ed8806ff5e0c1198c95f32c1972675
getzch <keepprogress@gmail.com>
2021-01-01 20:57:49 +0800

    create REST Controller

--- src/main/java/com/luv2code/springdemo/entity/Student.java
+++ src/main/java/com/luv2code/springdemo/entity/Student.java
@@ -9,4 +9,26 @@ public class Student {
 		
 	}
 
+	public Student(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	
 }
